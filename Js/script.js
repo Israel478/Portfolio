c6f7388562935c2ec64f10266a99acb16bc3b28e
Israel <israelbekeletulu@gmail>
2025-01-10 13:15:48 +0300

    Task: changes

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,17 @@
+function sendemail(event){
+
+  const username= document.getElementById("name").value
+  const email= document.getElementById("email").value
+  const subject= document.getElementById("subject").value
+  const message= document.getElementById("message").value
+  const link =document.createElement('a')
+  link.href= "mailto:israelbekeletulu@gmail.com?subject="+subject+"&body="+encodeURIComponent(message +"\n\n"+email)
+  link.target = "_blank"
+  link.click()
+}
+document.getElementById('submit').addEventListener('click', sendemail)
+
+
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
